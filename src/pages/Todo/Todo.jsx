3919f8f7d856b31e4c3555fbd0898372214dbe07
kimmoonju-102 <kimmun3549@naver.com>
2023-04-19 17:07:27 +0900

    [feat] todo 기능 구현
    - 할 일 추가
    - 할 일 수정과 삭제 구현
    - 로컬스토리지에 저장

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import styled from "styled-components/macro";
 import { CustomButton } from "@/components/Button/CustomButton";
 import { InputWithLabel } from "@/components/InputWithLabel/InputWithLabel";
@@ -6,10 +6,86 @@ import { InputWithLabel } from "@/components/InputWithLabel/InputWithLabel";
 export function Todo() {
   const [tasks, setTasks] = useState([]); // 할 일 목록을 저장할 배열
   const [editingTaskId, setEditingTaskId] = useState(null);
+  const [editingTaskName, setEditingTaskName] = useState("");
+
+  useEffect(() => {
+    const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks") || "[]");
+    setTasks(tasksFromLocalStorage);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // 입력한 할 일을 추가하는 함수
+  const addTask = (task) => {
+    setTasks([...tasks, { id: Date.now(), name: task, completed: false }]);
+  };
+
+
+  // 선택한 할 일을 삭제하는 함수
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // 선택한 할 일의 이름을 수정하는 함수
+  const editTask = (id, newName) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, name: newName };
+        }
+        return task;
+      })
+    );
+  };
+
+  // 입력한 할 일을 처리하는 함수
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTaskName = event.target.elements.task.value;
+    if (newTaskName.trim() !== "") {
+      addTask(newTaskName);
+      event.target.reset();
+    }
+  };
+  
+  // 입력한 일 수정하는 함수
+  const handleEditTask = (task) => {
+    setEditingTaskId(task.id);
+    setEditingTaskName(task.name);
+  };
+
+  const handleCancelEdit = () => {
+    setEditingTaskId(null);
+    setEditingTaskName("");
+  };
+
+  const handleModifyInputChange = (event) => {
+    setEditingTaskName(event.target.value);
+  };
+
+  const handleModifySubmit = (event) => {
+    event.preventDefault();
+    editTask(editingTaskId, editingTaskName);
+    setEditingTaskId(null);
+    setEditingTaskName("");
+  };
+
+  const handleCheckboxChange = (id) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      })
+    );
+  };
 
   return (
     <StyledTodo>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Todo List</h1>
         <InputWithLabel
           data-testid="new-todo-input"
@@ -26,15 +102,17 @@ export function Todo() {
       {tasks.map((task) => (
           <li key={task.id}>
             {editingTaskId === task.id ? (
-              <form>
+              <form onSubmit={handleModifySubmit}>
                 <input
                   type="text"
+                  value={editingTaskName}
+                  onChange={handleModifyInputChange}
                   data-testid="modify-input"
                 />
                 <CustomButton data-testid="submit-button" type="submit">
                   수정
                 </CustomButton>
-                <CustomButton data-testid="cancel-button">
+                <CustomButton data-testid="cancel-button" onClick={handleCancelEdit}>
                   취소
                 </CustomButton>
               </form>
@@ -43,9 +121,11 @@ export function Todo() {
                 {task.name}
                 <input
                   type="checkbox"
+                  checked={task.completed}
+                  onChange={() => handleCheckboxChange(task.id)}
                 />
-                <CustomButton>삭제</CustomButton>
-                <CustomButton>수정</CustomButton>
+                <CustomButton onClick={() => deleteTask(task.id)}>삭제</CustomButton>
+                <CustomButton onClick={() => handleEditTask(task)}>수정</CustomButton>
               </>
             )}
           </li>
