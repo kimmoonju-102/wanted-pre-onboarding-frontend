59caae48af14cc8911811024e7ad56ead1f4aaaf
kimmoonju-102 <kimmun3549@naver.com>
2023-04-19 19:26:27 +0900

    [chore] 경로설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { Signup, Signin, Todo } from "@/pages";
+import { Home, Signup, Signin, Todo } from "@/pages";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/todos" element={<Todo />} />
       </Routes>
